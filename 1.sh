329e4effc2f1d1d4ff6e6ee4782833f9d219caab
S22F5 <s22f5@mailfence.com>
2022-12-20 17:50:47 +0100

    fixes

--- 1.sh
+++ 1.sh
@@ -11,7 +11,7 @@ convert 1/or.png -solarize 1 1/o.png
 
 
 ## split image into individual pixels
-convert 1/o.png -crop 400x1 1/%d.png
+convert 1/o.png -crop 9999x1 1/%d.png
 
 
 
@@ -19,26 +19,30 @@ convert 1/o.png -crop 400x1 1/%d.png
 rollimage () {
   roll=$(convert $1 -fuzz 58% -trim - | identify - | grep -Po '\+\K[^+]*'  | head -1)
   y=${1%.bar}
-  convert $1 -roll -$roll 2/rolled${y##*/}
+  convert $1 -roll -$roll 2/rolled${y##*/}.jpg
 }
 
+## cleanup, remove if you want summary image
+rm 1/or.png
+rm 1/o.png
 
 ## rollimages
 for f in 1/*.png; do
 	rollimage $f
   done
 
+## cleanup
+rm -Rvf 1/
 
 ## recreate image
-convert 2/rolled*.png -append  full_img.png
+convert 2/*.jpg -append  full_img.png
+convert -colorspace gray  -threshold 10% -resize 480% -sharpen 10 -negate full_img.png out.png
 
-
-##cleanup
-rm -Rvf 1/
+## cleanup
 rm -Rvf 2/
 
 
 
-feh full_img.png
+bash feh out.png
 printf "done"
 
